4220ebbd6d4b11e658d8219c56440e64eb08d7ba
JamesGray <bigbossjgw@gmail.com>
2020-12-04 16:32:18 +0800

    Lab4 1.0

--- kernel/file.c
+++ kernel/file.c
@@ -16,7 +16,7 @@
 struct devsw devsw[NDEV];
 struct {
   struct spinlock lock;
-  struct file file[NFILE];
+  //struct file file[NFILE];
 } ftable;
 
 void
@@ -32,13 +32,23 @@ filealloc(void)
   struct file *f;
 
   acquire(&ftable.lock);
-  for(f = ftable.file; f < ftable.file + NFILE; f++){
+
+  f = bd_malloc(sizeof(struct file));//动态申请文件描述符
+  if(f){
+    memset(f, 0, sizeof(struct file));//内存初始化为0
     if(f->ref == 0){
       f->ref = 1;
       release(&ftable.lock);
       return f;
-    }
+      }
   }
+  // for(f = ftable.file; f < ftable.file + NFILE; f++){
+  //   if(f->ref == 0){
+  //     f->ref = 1;
+  //     release(&ftable.lock);
+  //     return f;
+  //   }
+  // }
   release(&ftable.lock);
   return 0;
 }
@@ -80,6 +90,7 @@ fileclose(struct file *f)
     iput(ff.ip);
     end_op(ff.ip->dev);
   }
+  bd_free(f);//释放文件描述符
 }
 
 // Get metadata about file f.
